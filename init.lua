8e8fec42cc68137c3a31ed5c12abc732bb2a1bdc
axel-paillaud <contact@axelweb.fr>
2023-09-17 21:15:11 +0200

    create after directory : after load plugin (thinks asynchronous)

--- init.lua
+++ init.lua
@@ -52,13 +52,6 @@ require'nvim-treesitter.configs'.setup {
   },
 }
 
--- Keymap for Telescope
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-
 -- nvim-tree
 -- set termguicolors to enable highlight groups
 vim.opt.termguicolors = true
