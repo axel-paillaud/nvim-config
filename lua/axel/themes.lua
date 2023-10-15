3d82ff600b3235544f37224271b5f485230f291e
axel-paillaud <contact@axelweb.fr>
2023-10-15 15:15:57 +0200

    setup theme override

--- lua/axel/themes.lua
+++ lua/axel/themes.lua
@@ -1,8 +1,11 @@
 -- Theme gruvbox
 require("gruvbox").setup({
+    overrides = {
+
+    }
 })
 vim.o.background = "light"
 vim.cmd([[colorscheme gruvbox]])
 
 -- set termguicolors to enable highlight groups
--- vim.opt.termguicolors = true 
+vim.opt.termguicolors = true 
