78bbbec47d4bfeb709c216b9c9d1d5e495be0a04
axel-paillaud <contact@axelweb.fr>
2023-10-15 10:52:36 +0200

    set hover() view dap to also visual mode

--- lua/axel/remap.lua
+++ lua/axel/remap.lua
@@ -5,13 +5,13 @@ vim.keymap.set('n', '<leader>ef', ':NvimTreeFocus<CR>')
 -- Keymap to control DAP. See :help dap-mappings for more idea
 local dap = require("dap")
 
-vim.keymap.set('n', '<F5>', require 'dap'.continue)
-vim.keymap.set('n', '<leader>so', require 'dap'.step_over)
-vim.keymap.set('n', '<leader>sin', require 'dap'.step_into)
-vim.keymap.set('n', '<leader>sout', require 'dap'.step_out)
+vim.keymap.set('n', '<F5>', function() require('dap').continue() end)
+vim.keymap.set('n', '<F10>', function() require('dap').step_over() end)
+vim.keymap.set('n', '<F11>', function() require('dap').step_into() end)
+vim.keymap.set('n', '<F12>', function() require('dap').step_out() end)
 vim.keymap.set('n', '<leader>b', require 'dap'.toggle_breakpoint)
 vim.keymap.set('n', '<leader>B', function()
     require 'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))
 end)
 -- View the value for the expression under the cursor in a floating window :
-vim.keymap.set('n', '<leader>v', require 'dap.ui.widgets'.hover)
+vim.keymap.set({'n', 'v'}, '<leader>v', require 'dap.ui.widgets'.hover)
