8dc39371931052f104cb731fab6fc3050ae2f02b
axel-paillaud <contact@axelweb.fr>
2023-10-15 11:50:30 +0200

    add jest DAP support config, work well

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -44,6 +44,21 @@ for _, language in ipairs({ "typescript", "javascript" }) do
             webRoot = "${workspaceFolder}",
             userDataDir = "${workspaceFolder}/.vscode/vscode-chrome-debug-userdatadir"
         },
+        {
+            type = "pwa-node",
+            request = "launch",
+            name = "Debug Jest Tests",
+            -- trace = true, -- include debugger info
+            runtimeExecutable = "node",
+            runtimeArgs = {
+                "./node_modules/jest/bin/jest.js",
+                "--runInBand",
+            },
+            rootPath = "${workspaceFolder}",
+            cwd = "${workspaceFolder}",
+            console = "integratedTerminal",
+            internalConsoleOptions = "neverOpen",
+        },
     }
 end
 
