8e687fcf57c5a39d22934dfac2bc65d6951904ff
axel-paillaud <contact@axelweb.fr>
2023-10-16 08:26:55 +0200

    update breakpoint icon, now follow gruvbox theme

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -1,4 +1,17 @@
-vim.fn.sign_define('DapBreakpoint', {text='', texthl='', linehl='', numhl=''})
+vim.fn.sign_define('DapBreakpoint', 
+    {
+        text='', 
+        texthl='DapBreakpointSymbol', 
+        linehl='DapBreakpoint', 
+        numhl='DapBreakpoint'
+    })
+vim.fn.sign_define('DapStopped', 
+    {
+        text='', 
+        texthl='DapStoppedSymbol', 
+        linehl='DapBreakpoint', 
+        numhl='DapBreakpoint'
+    })
 
 -- Microsoft JavaScript Debugger setup
 require("dap-vscode-js").setup({
