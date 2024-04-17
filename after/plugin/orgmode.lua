f14e6329d109aefdea16f515a06cde413486917c
axel-paillaud <contact@axelweb.fr>
2024-04-17 06:22:52 +0200

    remove require('orgmode').setup_ts_grammar(), dependency on nvim-treesitter was removed

--- after/plugin/orgmode.lua
+++ after/plugin/orgmode.lua
@@ -1,6 +1,4 @@
 -- Org-mode
--- Load custom treesitter grammar for org filetype
-require('orgmode').setup_ts_grammar()
 
 require('orgmode').setup({
   org_agenda_files = {'~/Dev/org/**/*', '~/my-orgs/**/*'},
