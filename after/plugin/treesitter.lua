efd073f6d47aad80dbfc4bd46367e0426c4e5ece
axel-paillaud <contact@axelweb.fr>
2024-04-27 12:26:17 +0200

    remove latex nvim-treesitter parser, cause me an error at nvim launch

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,17 +1,26 @@
-require'nvim-treesitter.configs'.setup {
-    ensure_installed = {
-        'org', 'javascript', 'vim', 'vimdoc', 'html', 'php', 'vue', 'c', 'lua',
-        'hlsl', 'json', 'latex', 'http', 'css', 'scss', 'typescript', 'vala',
-        'xml',
-    },
-  highlight = {
-    enable = true,
-    -- Required for spellcheck, some LaTex highlights and
-    -- code block highlights that do not have ts grammar
-    additional_vim_regex_highlighting = {'org'},
-  },
-  indent = {
-    enable = true,
-  },
-}
+-- april 2024 : latex parser cause me an error at nvim launch
 
+require'nvim-treesitter.configs'.setup {
+    ensure_installed = { 
+        "c", "lua", "vim", "vimdoc", "query", "org", "javascript", "html", "php", 
+        "hlsl", "json", "http", "css", "scss", "typescript", "vala", "xml", "vue",
+    },
+
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = false,
+
+    -- Automatically install missing parsers when entering buffer
+    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+    auto_install = true,
+
+    highlight = {
+        enable = true,
+
+        -- Required for spellcheck, some LaTex highlights and
+        -- code block highlights that do not have ts grammar
+        additional_vim_regex_highlighting = {"org"},
+    },
+    indent = {
+        enable = true,
+    },
+} 
