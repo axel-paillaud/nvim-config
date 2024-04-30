ea92c624e3275a10989de33d289050a4b3b3719b
axel-paillaud <contact@axelweb.fr>
2024-04-30 21:46:58 +0200

    reload set.lua

--- init.lua
+++ init.lua
@@ -2,6 +2,8 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+require("set")
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
