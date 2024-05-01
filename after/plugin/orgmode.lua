cb88a60c20d70260a81eb236c99acf34418d9f79
axel-paillaud <contact@axelweb.fr>
2024-05-01 20:33:59 +0200

    nvim-orgmode : set default notes file to /org/refile instead of ~/dev/org/refile, same with agenda file

--- after/plugin/orgmode.lua
+++ after/plugin/orgmode.lua
@@ -1,7 +1,7 @@
 -- Org-mode
 
 require('orgmode').setup({
-  org_agenda_files = {'~/Dev/org/**/*', '~/my-orgs/**/*'},
-  org_default_notes_file = '~/Sync/org/refile.org',
+  org_agenda_files = {'~/org/**/*', '~/my-orgs/**/*'},
+  org_default_notes_file = '~/org/refile.org',
 })
 
