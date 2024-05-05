753dc389cab3339cca7c103ab826974da4659406
axel-paillaud <contact@axelweb.fr>
2024-05-05 18:25:37 +0200

    remove trailling space on init.lua file

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -1,15 +1,15 @@
-vim.fn.sign_define('DapBreakpoint', 
+vim.fn.sign_define('DapBreakpoint',
     {
-        text='', 
-        texthl='DapBreakpointSymbol', 
-        linehl='DapBreakpoint', 
+        text='',
+        texthl='DapBreakpointSymbol',
+        linehl='DapBreakpoint',
         numhl='DapBreakpoint'
     })
-vim.fn.sign_define('DapStopped', 
+vim.fn.sign_define('DapStopped',
     {
-        text='', 
-        texthl='DapStoppedSymbol', 
-        linehl='DapBreakpoint', 
+        text='',
+        texthl='DapStoppedSymbol',
+        linehl='DapBreakpoint',
         numhl='DapBreakpoint'
     })
 
