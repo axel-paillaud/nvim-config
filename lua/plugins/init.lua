514f1fd434b4ebc00594365f285174b2f94aeb9b
axel-paillaud <contact@axelweb.fr>
2024-05-12 18:50:43 +0200

    delete (finally) after file with lsp config. remove comment in init.lua file

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,8 +1,4 @@
 return {
-    -- {'hrsh7th/nvim-cmp'},
-    -- {'L3MON4D3/LuaSnip'},
-    -- {'hrsh7th/cmp-nvim-lsp'},
     "mfussenegger/nvim-dap",
     "mxsdev/nvim-dap-vscode-js",
-    --{"microsoft/vscode-js-debug", build = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out"},
 }
