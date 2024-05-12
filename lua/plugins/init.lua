8797ffc469d54e5624a947e36ab9260ff593ab4d
axel-paillaud <contact@axelweb.fr>
2024-05-12 18:11:17 +0200

    change order

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,7 +1,7 @@
 return {
-    {'hrsh7th/cmp-nvim-lsp'},
     {'hrsh7th/nvim-cmp'},
     {'L3MON4D3/LuaSnip'},
+    {'hrsh7th/cmp-nvim-lsp'},
     "mfussenegger/nvim-dap",
     "mxsdev/nvim-dap-vscode-js",
     --{"microsoft/vscode-js-debug", build = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out"},
