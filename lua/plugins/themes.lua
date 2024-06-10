a5d0cb43374ba838b12345d7aca7b85c80438132
axel-paillaud <contact@axelweb.fr>
2024-06-10 17:16:58 +0200

    remove useless config on gruvbox theme

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,17 +1,13 @@
 return {
-    "ellisonleao/gruvbox.nvim", 
-    priority = 1000, 
-    config = true, 
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
     lazy = false,
     config = function()
-
         -- "light" or "dark" theme
         vim.o.background = "dark"
-        vim.cmd([[colorscheme gruvbox]]) 
-
+        vim.cmd([[colorscheme gruvbox]])
 
         -- set termguicolors to enable highlight groups
-        vim.opt.termguicolors = true 
-
+        vim.opt.termguicolors = true
     end
 }
