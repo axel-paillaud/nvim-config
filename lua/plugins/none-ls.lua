448667ddc094b5996e7469cb3493ad69b11c67f9
axel-paillaud <contact@axelweb.fr>
2024-06-10 16:51:34 +0200

    update

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -4,6 +4,7 @@ return {
         local null_ls = require("null-ls")
 
         null_ls.setup({
+            debug = true,
             sources = {
                 null_ls.builtins.formatting.stylua,
                 null_ls.builtins.diagnostics.markuplint.with({
