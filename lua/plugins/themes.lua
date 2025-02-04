ddb4e8f2f50fb37a3040dd39bedc205496f600c0
axel-paillaud <contact@axelweb.fr>
2025-02-04 10:09:48 +0100

    try new gruvbox theme

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,13 +1,25 @@
-return {
-    "ellisonleao/gruvbox.nvim",
-    priority = 1000,
-    lazy = false,
-    config = function()
+-- return {
+    -- "ellisonleao/gruvbox.nvim",
+    -- priority = 1000,
+    -- lazy = false,
+    -- config = function()
         -- "light" or "dark" theme
-        vim.o.background = "dark"
-        vim.cmd([[colorscheme gruvbox]])
+        -- vim.o.background = "dark"
+        -- vim.cmd([[colorscheme gruvbox]])
 
         -- set termguicolors to enable highlight groups
-        vim.opt.termguicolors = true
+        -- vim.opt.termguicolors = true
+    -- end
+-- }
+
+return {
+    'sainnhe/gruvbox-material',
+    lazy = false,
+    priority = 1000,
+    config = function()
+        -- Optionally configure and load the colorscheme
+        -- directly inside the plugin declaration.
+        vim.g.gruvbox_material_enable_italic = true
+        vim.cmd.colorscheme('gruvbox-material')
     end
 }
