566ad2f346c770fa3016a3631f46ba45d0c5aa16
axel-paillaud <contact@axelweb.fr>
2025-03-04 14:11:06 +0100

    update gruvbox material to have light theme

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -21,5 +21,6 @@ return {
         -- directly inside the plugin declaration.
         vim.g.gruvbox_material_enable_italic = true
         vim.cmd.colorscheme('gruvbox-material')
+        vim.o.background = 'light'
     end
 }
